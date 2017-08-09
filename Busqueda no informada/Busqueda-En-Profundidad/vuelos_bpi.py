9e5aad8606bd5bdbc101dcee033ca21201a8be9f
Jose Manuel Gama <josegamaes@gmail.com>
2017-08-08 23:54:08 -0500

    continuacion

--- Busqueda no informada/Busqueda-En-Profundidad/vuelos_bpi.py
+++ Busqueda no informada/Busqueda-En-Profundidad/vuelos_bpi.py
@@ -14,3 +14,21 @@ def buscar_solucion_DFS_Rec(nodo, solucion, visitados, limite):
 		if nodo.get_datos == solucion:
 			return nodo
 		else:
+			# expandir nodos hijo (ciudades con conexiones)
+			dato_nodo = nodo.get_datos()
+			lista_hijos = []
+			for un_hijo in conexiones[dato_nodo]:
+				hijo = Nodo(un_hijo)
+				if not hijo.en_lista(visitados):
+					lista_hijos.append(hijo)
+
+			nodo.set_hijos(lista_hijos)
+
+			for nodo_hijo in nodo.get_hijos():
+				if not nodo_hijo.get_datos() in visitados:
+					# llamada recursiva
+					sol = buscar_solucion_DFS_Rec(nodo_hijo, solucion, visitados, limite-1)
+					if sol != None:
+						return sol
+
+		return None
