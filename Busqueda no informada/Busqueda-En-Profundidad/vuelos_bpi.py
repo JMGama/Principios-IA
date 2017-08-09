6e822c70ec9a0688471e11e2f323177be5652d69
Jose Manuel Gama <josegamaes@gmail.com>
2017-08-08 21:27:45 -0500

    continuacion

--- Busqueda no informada/Busqueda-En-Profundidad/vuelos_bpi.py
+++ Busqueda no informada/Busqueda-En-Profundidad/vuelos_bpi.py
@@ -5,4 +5,12 @@ def DFS_prof_iter(nodo, solucion):
 	for limite in range(0,100):
 		visitados=[]
 		sol = buscar_solucion_DFS_Rec(nodo, solucion, visitados, limite)
-		
+		if sol != None:
+			return sol
+
+def buscar_solucion_DFS_Rec(nodo, solucion, visitados, limite):
+	if limite > 0:
+		visitados.append(nodo)
+		if nodo.get_datos == solucion:
+			return nodo
+		else:
